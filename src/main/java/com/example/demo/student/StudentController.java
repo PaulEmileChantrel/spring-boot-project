f0be4f59bfc16791c0106a473601ca7f9dbec6e6
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-10-21 23:15:18 -0400

    setting up database

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -12,6 +14,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,9 +2,13 @@ package com.example.demo.student;
 
 
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 
+@Service
 public class StudentService {
     public List<Student> getStudent() {
         return List.of(
